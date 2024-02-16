82fc243f823253227aecfce29357cc4467cf4d2b
HLB288 <henrilb.hlb@gmail.com>
2024-02-16 18:04:26 +0100

    ETH Wallet commit

--- EtherWallet.sol
+++ EtherWallet.sol
@@ -1,3 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.16;
 
+contract EtherWallet {
+
+    address payable public owner;
+
+    constructor (address payable _owner)  {
+        owner = _owner;
+    }
+    function deposit() payable public {
+
+    }
+
+    function withdraw(address payable receiver, uint amount) public {
+        require(msg.sender == owner, "Only owner can withdraw the Ether");
+        receiver.transfer(amount);
+    }
+
+    function balanceOf() view public returns(uint) {
+        return address(this).balance;
+    }
+
+   
+}
